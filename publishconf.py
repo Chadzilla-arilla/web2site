cf6aeb586253ce7091ac7980c1b2b3c81b418bb3
Chadzilla-arilla <112816792+Chadzilla-arilla@users.noreply.github.com>
2025-11-26 15:15:36 -0500

    next

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://yourusername.github.io/web2site"
+SITEURL = "https://chadzilla-arilla.github.io/web2site"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
